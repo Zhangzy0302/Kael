07c2ce2826ed4cbb20999bacf19166487b7a3a0c
zzy <2218847957@qq.com>
2026-03-31 18:16:48 +0800

    Initial Commit

---
+++ Kael/KaelApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaelApp.swift
+//  Kael
+//
+//  Created by yangyang on 2026/3/31.
+//
+
+import SwiftUI
+
+@main
+struct KaelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
